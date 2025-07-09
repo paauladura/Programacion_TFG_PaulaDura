31d24317887b38c362d2f760e2856d9fc453d7b0
paauladura <pauladfuster@gmail.com>
2025-07-09 23:23:51 +0200

    Programacion_TFG_PaulaDura

--- modelos.m
+++ modelos.m
@@ -1,41 +1,24 @@
 %-------------------------------------------------------------------------------
-% GRACE Hackweek 2 (26-30 September, 2022)
-%
-%-------------------------------------------------------------------------------
-% Lab02: Gravity field theory: Spherical harmonic analysis
-% Instructor: Prof. Roland Pail
-% TA: K. Bharath N. Reddy
-%     Digvijay Singh
-%
-%-------------------------------------------------------------------------------
 % Add path for SHbundle, gracebundle, uberall software packages 
 addpath(genpath('/Users/paula/OneDrive/Escritorio/4º CUATRI 2/TFG/CosasApasar'))
 %
 %-------------------------------------------------------------------------------
-%% Q1 Disturbing  potential
-%  
-%% i. Import the static gravity field model ITSG-Grace2018s with readicgem. 
+% 
+%% Import the static gravity field model ITSG-Grace2018s with readicgem. 
    [scs, ncs, header, scst, ncst]  = readicgem('ITSG-Grace2018s.gfc');
-   
-%% ii. Generate disturbing  potential  coefficients  by  reducing 
-% the  GRS80  normal  potential (normalklm)  from  the  given model .
-  nklm = normalklm (200,'grs80');
   
-%% iii. Plot the coefficients static potential, normal potential and 
-% disturbing  potential (sctriplot)
-  dis = scs - nklm;
 %-------------------------------------------------------------------------------
 
-%% Q2 Geoid Heights
+%% Geoid Heights
 %
-% i. Compute global grid of geoid heights up to various maximum harmonic degrees 
-% (e.g. nmax = 10, 50, 100, 200) with gshs_. 
+% Compute global grid of geoid heights up to various maximum harmonic degrees 
+% with gshs_. 
  [geoid_f_10, theRAD_10, lamRAD_10] = gshs_(dis,'quant','geoid','max_lm',10,'sub_WGS84',false);
  [geoid_f_50, theRAD_50, lamRAD_50] = gshs_(dis,'quant','geoid','max_lm',50,'sub_WGS84',false);
  [geoid_f_100, theRAD_100, lamRAD_100] = gshs_(dis,'quant','geoid','max_lm',100,'sub_WGS84',false);
  [geoid_f_200, theRAD_200, lamRAD_200] = gshs_(dis,'quant','geoid','max_lm',200,'sub_WGS84',false);
  
-%% ii. Plot the results (including coastlines) and interpret them. (mapfield) 
+%% Plot the results 
 mapfield(geoid_f_10,'pole',1);
 title('n=10', 'FontSize',30)
 colormap('jet')
@@ -74,9 +57,9 @@ set(gca, 'FontSize', 20) ;
  % As the degree of the harmonics go up, the results are more detailed.
  
 
-%% Q4 Gravity Disurbance 
+%% Potential
 %
-% i. Compute gravity disturbances up to nmax = 200 as seen from different 
+% Compute the potential up to nmax = 200 as seen from different 
 % satellite altitudes (e.g. h = 250, 450, 1000, 20000km). 
 [tr_f_200_h_250, theRAD_200, lamRAD_200] = gshs_(dis,'quant','potential','max_lm',200,...
                                       'sub_WGS84',false,'height',250*1000);
@@ -90,7 +73,7 @@ set(gca, 'FontSize', 20) ;
  [tr_f_200_h_20k theRAD_200, lamRAD_200] = gshs_(dis,'quant','potential','max_lm',200,...
                                       'sub_WGS84',false,'height',250*20000);  
                                   
-%% ii. Visualize the results in terms of global grids and interpret them. 
+%% Visualize the results in terms of global grids 
 mapfield(tr_f_200_h_250,'pole',1);
 title('h = 250km', 'FontSize',30)
 colormap('jet')
@@ -127,22 +110,4 @@ cb.Label.FontSize = 27;
 cb.Label.FontWeight = 'bold';
 set(gca, 'FontSize', 20) ;
 
-% iii. Based on findings so far, which conclusions can be drawn for a real
-% gravity field mission?
-% 
-% The areas with more gravity distubances are the ones where there is more
-% precipitation.
 %-------------------------------------------------------------------------------
-
-%%
-[geoid_f_200_2000, theRAD_200, lamRAD_200] = gshs_(dis,'quant','geoid','max_lm',200,'sub_WGS84',false,'height',0);
-
- mapfield(geoid_f_200_2000,'pole',1);
- title('n=200, h = 0')
- colormap(jet)
-
- [geoid_f_200_2000, theRAD_200, lamRAD_200] = gshs_(dis,'quant','geoid','max_lm',200,'sub_WGS84',false,'height',2000);
-
- mapfield(geoid_f_200_2000,'pole',1);
- title('n=200, h = 2000')
- colormap(jet)
